9a835a368843ebf8eb836f58ed89f446726ebaf6
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-02 01:38:54 +0200

    Adding mapping extensions.

---
+++ AlzaTestProject.Services/Extensions/ModelExtensions.cs
@@ -0,0 +1,21 @@
+﻿using AlzaTestProject.Domain.Models;
+using AlzaTestProject.Services.Dtos;
+
+namespace AlzaTestProject.Services.Extensions
+{
+	public static class ModelExtensions
+	{
+		public static ProductDto MapToDto(this Product product)
+		{
+			return new()
+			{
+				Id = product.Id,
+				Name = product.Name,
+				Description = product.Description ?? string.Empty,
+				Price = product.Price,
+				ImageUrl = product.ImageUrl.AbsoluteUri,
+				Stock = product.Stock,
+			};
+		}
+	}
+}
