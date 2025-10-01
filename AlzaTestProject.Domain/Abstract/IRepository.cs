68d99c42d22060650128864c9a463fb9e1e0a782
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-01 21:59:22 +0200

    Adding repository interface.

---
+++ AlzaTestProject.Domain/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.Domain.Abstract
+{
+	public interface IRepository<T>
+	{
+		Task<IEnumerable<T>> GetAll();
+		Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> predicate);
+		Task<T> GetById(int id);
+
+		Task<T> Create(T item);
+		Task<bool> Update(T item);
+		Task<bool> Delete(T item);
+	}
+}
