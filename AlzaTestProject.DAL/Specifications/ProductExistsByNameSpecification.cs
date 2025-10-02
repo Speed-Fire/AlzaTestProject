f66af316a8adca5201891815881a0175823604bc
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 00:22:04 +0200

    Adding specification for checking existance of product by name.

---
+++ AlzaTestProject.DAL/Specifications/ProductExistsByNameSpecification.cs
@@ -0,0 +1,23 @@
+﻿using AlzaTestProject.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.DAL.Specifications
+{
+	internal class ProductExistsByNameSpecification : IOrmSpecification<ProductEntity>
+	{
+		private readonly string _name;
+
+		public ProductExistsByNameSpecification(string name)
+		{
+			_name = name;
+		}
+
+		public Expression<Func<ProductEntity, bool>> Criteria =>
+			producctEntity => producctEntity.Name == _name;
+	}
+}
