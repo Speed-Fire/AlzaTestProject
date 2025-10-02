a7eb33769e0f9858808306b35bffa71787a73f22
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 00:22:38 +0200

    Adding product paging specification.

---
+++ AlzaTestProject.DAL/Specifications/ProductGetAllPagedSpecification.cs
@@ -0,0 +1,24 @@
+﻿using AlzaTestProject.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.DAL.Specifications
+{
+	internal class ProductGetAllPagedSpecification : IOrmSpecification<ProductEntity>
+	{
+		private readonly int _page;
+		private readonly int _pageSize;
+
+		public ProductGetAllPagedSpecification(int page, int pageSize)
+		{
+			_page = page;
+			_pageSize = pageSize;
+		}
+
+		public Expression<Func<ProductEntity, bool>> Criteria => throw new NotImplementedException();
+	}
+}
