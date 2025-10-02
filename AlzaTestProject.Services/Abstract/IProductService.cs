d3f2074578c2477303a6699b8c6a5ad86a04d37f
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-02 02:05:44 +0200

    Adding ProductService.

---
+++ AlzaTestProject.Services/Abstract/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using AlzaTestProject.Domain.Models;
+using AlzaTestProject.Services.Dtos;
+using OneOf;
+using OneOf.Types;
+
+namespace AlzaTestProject.Services.Abstract
+{
+	public interface IProductService
+	{
+		Task<OneOf<ProductDto, Error<string>>> CreateAsync(CreateProductDto createProductDto);
+		Task<IEnumerable<ProductDto>> GetAllAsync();
+		Task<OneOf<ProductDto, NotFound>> GetByIdAsync(int id);
+		Task<OneOf<ProductDto, NotFound, Error<string>>> UpdateStockAsync(int id, UpdateStockDto updateStockDto);
+	}
+}
---
+++ AlzaTestProject.Services/ProductService.cs
@@ -0,0 +1,77 @@
+﻿using AlzaTestProject.Domain.Abstract;
+using AlzaTestProject.Domain.Models;
+using AlzaTestProject.Services.Abstract;
+using AlzaTestProject.Services.Dtos;
+using AlzaTestProject.Services.Extensions;
+using OneOf;
+using OneOf.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AlzaTestProject.Services
+{
+	public class ProductService : IProductService
+	{
+		private readonly IRepository<Product> _productsRepository;
+
+		public ProductService(IRepository<Product> productsRepository)
+		{
+			_productsRepository = productsRepository;
+		}
+
+		public async Task<IEnumerable<ProductDto>> GetAllAsync()
+		{
+			return (await _productsRepository.GetAll()).Select(p => p.MapToDto());
+		}
+
+		public async Task<OneOf<ProductDto, NotFound>> GetByIdAsync(int id)
+		{
+			var product = await _productsRepository.GetById(id);
+			if (product is null)
+				return new NotFound();
+
+			return product.MapToDto();
+		}
+
+		public async Task<OneOf<ProductDto, Error<string>>> CreateAsync(CreateProductDto createProductDto)
+		{
+			try
+			{
+				if (await _productsRepository.Exists(p => p.Name == createProductDto.Name))
+					return new Error<string>("Product with the same name already exists.");
+
+				var product = new Product(createProductDto.Name, createProductDto.ImageUrl);
+				product = await _productsRepository.Create(product);
+
+				return product.MapToDto();
+			}
+			catch (ArgumentException ex)
+			{
+				return new Error<string>(ex.Message);
+			}
+		}
+
+		public async Task<OneOf<ProductDto, NotFound, Error<string>>> UpdateStockAsync(int id, UpdateStockDto updateStockDto)
+		{
+			try
+			{
+				var product = await _productsRepository.GetById(id);
+				if (product is null)
+					return new NotFound();
+
+				product.UpdateStock(updateStockDto.NewStock);
+				product = await _productsRepository.Update(product);
+
+				return product.MapToDto();
+			}
+			catch (ArgumentException ex)
+			{
+				return new Error<string>(ex.Message);
+			}
+		}
+	}
+}
