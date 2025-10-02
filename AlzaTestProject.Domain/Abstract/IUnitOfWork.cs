9f72aff7a1142a8c69206f0a90b5a9c3d856db66
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 01:18:43 +0200

    Adding unit of work interface.

---
+++ AlzaTestProject.Domain/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.Domain.Abstract
+{
+	public interface IUnitOfWork : IDisposable, IAsyncDisposable
+	{
+		IRepository<TModel> GetRepository<TModel>();
+		Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+		Task BeginTransaction(CancellationToken cancellationToken = default);
+		Task CommitTransaction(CancellationToken cancellationToken = default);
+		Task RollbackTransaction(CancellationToken cancellationToken = default);
+	}
+}
