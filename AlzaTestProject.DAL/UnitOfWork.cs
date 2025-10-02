57c7bd1998fc09b5be900c840b66d91d4ddc30b6
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 01:18:57 +0200

    Adding unit of work implementation.

---
+++ AlzaTestProject.DAL/UnitOfWork.cs
@@ -0,0 +1,91 @@
+﻿using AlzaTestProject.DAL.Contextes;
+using AlzaTestProject.Domain.Abstract;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.DAL
+{
+	public sealed class UnitOfWork : IUnitOfWork
+	{
+		private readonly AppDbContext _dbContext;
+		private readonly Dictionary<Type, object> _repositories = [];
+		private readonly IServiceProvider _serviceProvider;
+
+		private IDbContextTransaction? _transaction;
+
+		public UnitOfWork(AppDbContext dbContext, IServiceProvider serviceProvider)
+		{
+			_dbContext = dbContext;
+			_serviceProvider = serviceProvider;
+		}
+
+		public IRepository<TModel> GetRepository<TModel>()
+		{
+			var type = typeof(TModel);
+
+			if(_repositories.TryGetValue(type, out var repository))
+			{
+				return (IRepository<TModel>)repository;
+			}
+
+			repository = _serviceProvider.GetRequiredService<IRepository<TModel>>();
+			_repositories[type] = repository;
+
+			return (IRepository<TModel>)repository;
+		}
+
+		public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			return _dbContext.SaveChangesAsync(cancellationToken);
+		}
+
+		public Task BeginTransaction(CancellationToken cancellationToken = default)
+		{
+			if (_transaction is null)
+				return _dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+			return Task.CompletedTask;
+		}
+
+		public async Task CommitTransaction(CancellationToken cancellationToken = default)
+		{
+			if (_transaction is null)
+				throw new InvalidOperationException("Trying to commit not existing transaction.");
+
+			await _dbContext.SaveChangesAsync(cancellationToken);
+			await _transaction.CommitAsync(cancellationToken);
+			await _transaction.DisposeAsync();
+
+			_transaction = null;
+		}
+
+		public async Task RollbackTransaction(CancellationToken cancellationToken = default)
+		{
+			if (_transaction is null)
+				throw new InvalidOperationException("Trying to rollback not existing transaction.");
+
+			await _transaction.RollbackAsync(cancellationToken);
+			await _transaction.DisposeAsync();
+
+			_transaction = null;
+		}
+
+		public void Dispose()
+		{
+			_transaction?.Dispose();
+		}
+
+		public ValueTask DisposeAsync()
+		{
+			if (_transaction is not null)
+				return _transaction.DisposeAsync();
+
+			return ValueTask.CompletedTask;
+		}
+	}
+}
