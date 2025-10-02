626eeb4cee407fdf793061b65b2d0ec71ceceac9
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 00:23:52 +0200

    Adding product specification factory implementation.

---
+++ AlzaTestProject.DAL/Specifications/Factories/ProductSpecificationFactory.cs
@@ -0,0 +1,22 @@
+﻿using AlzaTestProject.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.DAL.Specifications.Factories
+{
+	internal class ProductSpecificationFactory : IProductSpecificationFactory
+	{
+		public ISpecification ExistsByNameSpecification(string name)
+		{
+			return new ProductExistsByNameSpecification(name);
+		}
+
+		public ISpecification GetAllPagedSpecificatyion(int page, int pageSize)
+		{
+			return new ProductGetAllPagedSpecification(page, pageSize);
+		}
+	}
+}
