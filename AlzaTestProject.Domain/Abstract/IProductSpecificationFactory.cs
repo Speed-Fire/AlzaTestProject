337ae9c7a51dd4d42414ddb6447d236663743c26
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 00:16:29 +0200

    Adding product specification factory interface.

---
+++ AlzaTestProject.Domain/Abstract/IProductSpecificationFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.Domain.Abstract
+{
+	public interface IProductSpecificationFactory
+	{
+		ISpecification ExistsByNameSpecification(string name);
+		ISpecification GetAllPagedSpecificatyion(int page, int pageSize);
+	}
+}
