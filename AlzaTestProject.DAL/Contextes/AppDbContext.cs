b8446a13c7471b545cb1b1b49a038cdbfecb6ce8
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 00:19:23 +0200

    Adding application db context.

---
+++ AlzaTestProject.DAL/Contextes/AppDbContext.cs
@@ -0,0 +1,48 @@
+﻿using AlzaTestProject.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.DAL.Contextes
+{
+	public class AppDbContext : DbContext
+	{
+		internal DbSet<ProductEntity> Products => Set<ProductEntity>();
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductEntity>()
+				.HasAlternateKey(p => p.Name);
+		}
+
+		public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			var entities = ChangeTracker
+				.Entries<BaseEntity>()
+				.Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+			var now = DateTime.UtcNow;
+
+			foreach(var entity in entities)
+			{
+				if(entity.State == EntityState.Added)
+				{
+					entity.Entity.Created = now;
+					entity.Entity.Updated = now;
+				}
+				else
+				{
+					// protection of updating creation time
+					entity.Property(p => p.Created).IsModified = false;
+					entity.Entity.Updated = now;
+				}
+			}
+
+			return base.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
