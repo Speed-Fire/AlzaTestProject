8217d358e8bd66eb1839d2b40df993c4193cc8b5
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 00:17:42 +0200

    Adding orm entities.

---
+++ AlzaTestProject.DAL/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.DAL.Entities
+{
+	internal abstract class BaseEntity
+	{
+		public DateTime Created { get; set; }
+		public DateTime Updated { get; set; }
+	}
+}
---
+++ AlzaTestProject.DAL/Entities/ProductEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.DAL.Entities
+{
+	internal class ProductEntity : BaseEntity
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string? Description { get; set; }
+		public string ImageUrl { get; set; } = string.Empty;
+		public decimal Price { get; set; }
+		public int Stock { get; set; }
+	}
+}
