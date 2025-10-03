9cc5e98ce7dd24d2e3ea1687af227d28b8d9275a
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-03 02:25:04 +0200

    Adding Services DI registration methods.

---
+++ AlzaTestProject.Services/Extensions/DIExtensions.cs
@@ -0,0 +1,20 @@
+﻿using AlzaTestProject.Services.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.Services.Extensions
+{
+	public static class DIExtensions
+	{
+		public static IServiceCollection AddAlzaTestProjectServices(this IServiceCollection services)
+		{
+			services.AddTransient<IProductService, ProductService>();
+
+			return services;
+		}
+	}
+}
