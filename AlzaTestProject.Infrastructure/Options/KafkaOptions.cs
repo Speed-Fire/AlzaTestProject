256fa7a973d79bb48adef38d374421a61f46c22c
Vladislav Sidorovich <vlad44812@gmail.com>
2025-10-04 03:32:34 +0200

    Adding options for Kafka queue.

---
+++ AlzaTestProject.Infrastructure/Options/KafkaOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.Services.Kafka.Options
+{
+	public class KafkaOptions
+	{
+		public string BootstrapServers { get; set; } = string.Empty;
+
+		public string? Username { get; set; }
+		public string? Password { get; set; }
+		public string? SaslMechanism { get; set; } = "PLAIN"; // PLAIN, SCRAM-SHA-256, SCRAM-SHA-512
+		public string? SecurityProtocol { get; set; } = "SaslPlaintext"; // or "SaslSsl"
+	}
+}
---
+++ AlzaTestProject.Infrastructure/Options/KafkaQueueOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTestProject.Services.Kafka.Options
+{
+	public class KafkaQueueOptions
+	{
+		public string Topic { get; set; } = string.Empty;
+		public string GroupId { get; set; } = string.Empty;
+	}
+}
